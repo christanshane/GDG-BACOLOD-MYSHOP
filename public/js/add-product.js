e8f40cb401d8dbd5b5ee5684d80643d203dfdee8
Christan Shane D. Plaza <cosmoTechnologist@yahoo.com>
2018-11-17 16:30:48 +0800

    MyShop v0.2
    Update:
    - Image Upload with URL

--- public/js/add-product.js
+++ public/js/add-product.js
@@ -4,6 +4,7 @@ function addProduct(){
 	firestore.settings(settings);
 
 	var products = firebase.firestore().collection('products');
+	var imageURL = 'http://cdn.onlinewebfonts.com/svg/img_90947.png';
 	
 
  $('#add-product').on('submit', function(e) {
@@ -16,7 +17,8 @@ function addProduct(){
 	products.add({
 		name: name,
 		description: description,
-		price: price
+		price: price,
+		imageURL: imageURL,
 	}).then(function (result) {
 		M.toast({html: name + ' has been added with ID ' + result.id , classes: 'green'});
 	});
@@ -26,8 +28,15 @@ function addProduct(){
 	var image = this.files[0];
 	console.log(image);
 	readURL(image);
-	firebase.storage().ref().child('images/' + image.name).put(image);
+	firebase.storage().ref().child('images/' + image.name).put(image)
+	.then(function(snap){
+		snap.ref.getDownloadURL().then(function(url) {
+		imageURL = url;
+		 M.toast({html: 'Image downloadable at '+ url});
+		});
+	});
  });
+
  
  function readURL(image) {
 	var reader = new FileReader();
